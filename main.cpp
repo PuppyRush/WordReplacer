474188477f738287a38d37d3a5ffe9c0bf0e5c96
PuppyRush <gooddaumi@naver.com>
2018-02-11 15:34:20 +0900

    modify.
    out error.
    trimleft

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 ﻿
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <direct.h>		//mkdir
 #include <Windows.h>
 #include <fstream>
 #include <istream>
@@ -18,41 +19,79 @@ string getFilename(const string str) {
 	return str;
 }
 
-int main(int argc, const char **argv) {
+wchar_t* toWChar(const char *str) {
 
-	if (argc <= 2 || (argc - 2) % 2 == 1) {
-		cerr << "illegal arguments. check this command" << endl << "[origin path],([old word],[new word]) , (,) ,..." << endl;
-		exit(0);
+	size_t len = strlen(str);
+	wchar_t *wc = new wchar_t[len + 1];
+
+	mbstowcs(wc, str, len);
+	wc[len] = NULL;
+	return wc;
+}
+
+void trimLeft(string &str) {
+
+	size_t count = 0;
+	auto c = str.begin();
+	while (!str.empty() && c != str.end()) {
+
+		switch (str[count]) {
+		case '?':
+			count++;
+			break;
+		default:
+			c = str.end()-1;
+		}
+		
+		c++;
 	}
+
+	str = str.substr(count, str.size() - count);
+}
+
+int main(int argc, const char **argv) {
 
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.precision(10);
 
+	if (argc <= 4 || (argc - 2) % 2 == 0) {
+		cout << "illegal arguments. check this command" << endl << "[origin path],[new path], ([old word],[new word]) , (,) ,..." << endl;
+		exit(0);
+	}
+
 	string path(argv[1]);
 	map<string,string> words;
-	for (int i = 2; i < argc; i+=2) {
+	for (int i = 3; i < argc; i+=2) {
 		words.insert(make_pair(string(argv[i]),string(argv[i+1])) );
 	}
 
-	string temppath = "c:/temp_src/";
-	temppath = temppath.append(getFilename(path));
+	string newpath = argv[2];
+	newpath = newpath.append("\\").append(getFilename(path));
 
-	wchar_t _path[BUF_SIZE];
-	wchar_t newpath[BUF_SIZE];
-	
-	mbstowcs(_path, path.c_str(), strlen(path.c_str()) + 1);
-	mbstowcs(newpath, temppath.c_str(), temppath.size() + 1);
-	if (!CopyFile(_path, newpath, false)) {
-		cerr << "Error: " << GetLastError() << endl;
-		cerr << "need a directory necessarily-> c:\temp_src. check this dir. close this program. bye." << endl;
+	if (_mkdir(argv[2]) == ENOENT)
+	{
+		cout << "fail in making directory";
+		exit(0);
+	}
+
+	trimLeft(newpath);
+	trimLeft(path);
+
+	auto w_path = toWChar(path.c_str());
+	auto w_newpath = toWChar(newpath.c_str());
+
+	if (!CopyFile(w_path, w_newpath, false)) {
+		cout << "Error: " << GetLastError() << endl;
+		cout << "cant find the file. close this program. bye." << endl;
 		exit(0);
 	}
 	
+	bool isFinded = false;
 	char buf[BUF_SIZE];
 	memset(buf, 0, BUF_SIZE);
 
-	ifstream fs(temppath, std::ifstream::in);
+	ifstream fs(newpath, std::ifstream::in);
 	ofstream os(path, std::ofstream::out);
 	fs.seekg(0, std::ios_base::beg);
 	bool isEof = false;
@@ -69,6 +108,7 @@ int main(int argc, const char **argv) {
 				memset(buf, 0, BUF_SIZE);
 				strcat_s(buf, BUF_SIZE, s.c_str());
 				it = words.erase(it);
+				isFinded = true;
 			}
 			it++;
 		}
@@ -77,6 +117,10 @@ int main(int argc, const char **argv) {
 		buf[endpos + 1] = 0;
 		os << buf;
 	}
-	cout << "success changing words. bye.\n";
-	cout << "note : a origin file was moved to c:\temp_src";
+	if (isFinded)
+		cout << "success changing words. bye.\n";
+	else
+		cout << "fail chainging words. cant find them or there was something errors."<<endl;
+	cout << "note : a origin file was moved for safe ( " << path.c_str() << " -> " << newpath.c_str() << endl;
 }
+
